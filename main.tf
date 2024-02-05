af9acf6ad024b09e9eed8840fa27f5f3c8983142
jbeemster <joshua.a.beemster@gmail.com>
2024-02-05 13:16:57 +0100

    Bump version to v2.2.8 (closes #27)

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
   module_version = "0.4.3"
 
   app_name    = "s3-loader"
-  app_version = "2.2.6"
+  app_version = var.app_version
 
   local_tags = {
     Name           = var.name
@@ -28,7 +28,7 @@ data "aws_caller_identity" "current" {}
 
 module "telemetry" {
   source  = "snowplow-devops/telemetry/snowplow"
-  version = "0.4.0"
+  version = "0.5.0"
 
   count = var.telemetry_enabled ? 1 : 0
 
@@ -269,8 +269,8 @@ locals {
   user_data = templatefile("${path.module}/templates/user-data.sh.tmpl", {
     accept_limited_use_license = var.accept_limited_use_license
 
-    config  = local.hocon
-    version = local.app_version
+    config_b64 = base64encode(local.hocon)
+    version    = local.app_version
 
     telemetry_script = join("", module.telemetry.*.amazon_linux_2_user_data)
 
